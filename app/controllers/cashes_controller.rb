08525270cf89d141627b4d253d3dc64e7c925a91
blfernandezGit <blfernandez@up.edu.ph>
2021-12-16 21:18:44 +0800

    add authentication for each model

--- app/controllers/cashes_controller.rb
+++ app/controllers/cashes_controller.rb
@@ -1,4 +1,5 @@
 class CashesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_cash, only: %i[ show edit update destroy ]
 
   # GET /cashes or /cashes.json
