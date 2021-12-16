f2ee40e5a2d9fa38cb9e06baf6165f4fcbd7f614
blfernandezGit <blfernandez@up.edu.ph>
2021-12-16 21:39:49 +0800

    add devise views and edit registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception, prepend: true
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :role, :status, :email, :password, :password_confirmation)}
+
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :password, :current_password, :reset_password_token)}
+        end
+
 end
