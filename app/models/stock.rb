13bb9a67a1b05673c62c8241c9531eeef090b395
blfernandezGit <blfernandez@up.edu.ph>
2021-12-19 23:23:05 +0800

    display stocks with price caching

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,14 @@
 class Stock < ApplicationRecord
+    require 'iex-ruby-client'
+
+    def price
+        @client = IEX::Api::Client.new(
+            publishable_token: 'pk_2e688a1cf8f444d3a05bc370ffe80a52',
+            secret_token: 'sk_2265ce2d60a54e578dd7b80fc729a0c7',
+            endpoint: 'https://cloud.iexapis.com/v1'
+        )
+        Rails.cache.fetch("#{cache_key_with_version}/price", expires_in: 24.hours) do
+            @client.quote(code).latest_price
+        end
+    end
 end
