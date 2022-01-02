0f2e0d3977ec62f393b9769981136e6b302a306f
blfernandezGit <blfernandez@up.edu.ph>
2022-01-02 17:14:29 +0800

    change after_create to after_save after bundle install

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :transactions, dependent: :destroy
   has_many :inventories, dependent: :destroy
 
-  after_create :add_user_cash_data
+  after_save :add_user_cash_data
 
   validates :full_name, presence: true
   ROLES = %w[trader admin].freeze
@@ -30,6 +30,6 @@ class User < ApplicationRecord
   private 
 
   def add_user_cash_data
-    @cashes = Cash.create(balance: 0, user_id: self.id)
+    @cashes = Cash.create!(balance: 0, user_id: self.id)
   end
 end
