1ccfbb8c5e158e915ff2df770ca35e368ed45c80
blfernandezGit <blfernandez@up.edu.ph>
2022-01-07 21:55:09 +0800

    channels

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -6,7 +6,8 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import "../channels/consumer.js"
+import "../channels/index.js"
 import '../js/bootstrap_js_files.js'  
 import '../js/test.js'  
 
